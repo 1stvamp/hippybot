4e2d56e3abce07054f4fd3573301c4cbe19fc4c0
Wesley Mason <wes@1stvamp.org>
2011-09-03 23:26:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+"""Installer for hippybot
+"""
+
+try:
+        from setuptools import setup, find_packages
+except ImportError:
+        from ez_setup import use_setuptools
+        use_setuptools()
+        from setuptools import setup, find_packages
+setup(
+    name='hippybot',
+    description='Python Hipchat bot',
+    version='1.0.0',
+    author='Wes Mason',
+    author_email='wes[at]1stvamp[dot]org',
+    url='http://github.com/1stvamp/hippybot',
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=open('requirements.txt').readlines(),
+    license='BSD'
+)
