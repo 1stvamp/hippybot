3c16a6f573fd18879db971ee1624ecb6691b9ba1
Wesley Mason <wes@1stvamp.org>
2011-09-04 01:44:09 +0100

    Scaffold for bot code

---
+++ hippybot/bot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+import sys
+from jabberbot import JabberBot, botcmd
+from configparser import ConfigParser
+from optparse import OptionParser
+
+class HippyBot(JabberBot):
+    pass
