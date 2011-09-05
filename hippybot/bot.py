e83de7acd7448e66d9f918b94ef43f19ff8eafce
Wesley Mason <wes@1stvamp.org>
2011-09-05 09:34:37 +0100

    Fixed configparser import error

--- hippybot/bot.py
+++ hippybot/bot.py
@@ -2,7 +2,7 @@
 import os
 import sys
 from jabberbot import JabberBot, botcmd
-from configparser import ConfigParser
+from ConfigParser import ConfigParser
 from optparse import OptionParser
 
 class HippyBot(JabberBot):
@@ -11,11 +11,11 @@ class HippyBot(JabberBot):
 def main():
     parser = OptionParser(usage="""usage: %prog [options]""")
 
-    parser.add_option("-c", "--config", dest="confing_path", help="Config file path")
+    parser.add_option("-c", "--config", dest="config_path", help="Config file path")
     (options, pos_args) = parser.parse_args()
 
     if not options.config_path:
-        print >> sys.stderr, 'Missing config file path'
+        print >> sys.stderr, 'ERROR: Missing config file path'
         return 1
     return 0
 
