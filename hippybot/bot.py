c3ceda29d1b923392269642135921c4d3c00d6b1
Wesley Mason <wes@1stvamp.org>
2011-09-05 09:32:55 +0100

    Added xmpppy to reqs

--- hippybot/bot.py
+++ hippybot/bot.py
@@ -7,3 +7,17 @@ from optparse import OptionParser
 
 class HippyBot(JabberBot):
     pass
+
+def main():
+    parser = OptionParser(usage="""usage: %prog [options]""")
+
+    parser.add_option("-c", "--config", dest="confing_path", help="Config file path")
+    (options, pos_args) = parser.parse_args()
+
+    if not options.config_path:
+        print >> sys.stderr, 'Missing config file path'
+        return 1
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
