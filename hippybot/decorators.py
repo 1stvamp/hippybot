eac374ef853f51c8fa1267696056f954ec9078a7
Wesley Mason <wes@1stvamp.org>
2011-10-05 18:05:37 +0100

    Add @wraps to directcmd decorator to preserve doctstring

--- hippybot/decorators.py
+++ hippybot/decorators.py
@@ -1,6 +1,8 @@
+from functools import wraps
 from jabberbot import botcmd
 
 def directcmd(func):
+    @wraps(func)
     def wrapper(self, origin, args):
         message = func(self, origin, args)
         username = unicode(origin.getFrom()).split('/')[1]
