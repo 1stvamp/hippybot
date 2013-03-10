a5a3f75824e1fbbbd663829910a0059413e625eb
Devon Jones <devon@knewton.com>
2013-03-10 16:16:45 -0600

    simplifying wave and making it work with people just typing \o/

--- hippybot/plugins/mexican_wave.py
+++ hippybot/plugins/mexican_wave.py
@@ -1,26 +1,20 @@
+from collections import Counter
 from hippybot.decorators import botcmd
 
 class Plugin(object):
     """HippyBot plugin to make the bot complete a mexican wave if 2 people in a
     row do the action "\o/".
     """
-    global_commands = ['mexican_wave']
-    command_aliases = {r'\o/': 'mexican_wave'}
-    counts = {}
+    global_commands = ['\o/', 'wave']
+    command_aliases = {'\o/': 'wave'}
+    counts = Counter()
     @botcmd
     def mexican_wave(self, mess, args):
         channel = unicode(mess.getFrom()).split('/')[0]
-
-        if channel not in self.counts:
-            self.counts[channel] = 0
-
-        if self.bot._last_message.strip() != 'mexican_wave':
-            self.counts[channel] = 0
         self.bot.log.info("\o/ %s" %self.counts[channel])
 
         if not self.bot.from_bot(mess):
             self.counts[channel] += 1
-
             if self.counts[channel] == 2:
                 self.counts[channel] = 0
                 return r'\o/'
